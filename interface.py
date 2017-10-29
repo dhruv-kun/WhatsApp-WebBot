ddd6e9fb2237a21044ef33c0bca03a7c2b28bcd0
Dhruv Kunjumon <dhruvsci30@gmail.com>
2017-10-29 22:32:18 +0530

    Feature add and minor bug fixes
    Now names can be retrieved from the contact list and a reference
    cheatsheet is shared to the Master Group.

--- interface.py
+++ interface.py
@@ -12,47 +12,14 @@ from selenium.common.exceptions import *
 from content import Content
 import xkcd
 import brainyquote
+import readersdigest
 
-# Dictionary of the people to send messages
-TARGETS = {
-    "BAK": "Badal DTU",
-    "ASK": "Ashutosh Kumar",
-    "AKD": "Akshay Kapoor DTU",
-    "MAK": "Mhd Ashar",
-    "SAC": "Sachit Bhai",
-    "KEC": "Keerti Chechi",
-    "SAK": "Sakshi",
-    "PRD": "Priya",
-    "NID": "Nikhil DTU A2",
-    "SOT": "Sonu Thakur",
-    "GID": "Gitesh DTU",
-    "MOD": "Mohit IT",
-    "DHD": "Dhruv Anand DTU",
-    "HID": "Hitesh Kumar A2",
-    "SAD": "Sachin DTU",
-    "NID": "Nikita DTU",
-    "AVD": "Ajay Verma DTU",
-    "AKE": "Akash ECE",
-    "RAB": "Ramya",
-    "KRD": "Krishan DTU",
-    "KAD": "Karan Goyal DTU",
-    "RAD": "Rajat Kumar DTU B2",
-    "SAS": "Saarthak",
-    "HES": "Hemant",
-    "KAN": "Kannan",
-    "REP": "Rejith Prabhakar",
-    "MAM": "Mamma",
-    "PAP": "PAP",
-    "VIN": "Vibhu",
-    "NEB": "Neha",
-    "LAD": "Lakhan Bakshi DTU",
-    "NIB": "Nibin",
-}
 
 # List of Websites from which the data is collected
 ALLSITES = [
-    "xkcd",
-    "brainyquote",
+    xkcd,
+    brainyquote,
+    readersdigest,
 ]
 
 
@@ -199,12 +166,19 @@ def recieve(driver, target='Slave'):
     #     A. P. J. Abdul Kalam
     #     PRD RAB SAS
     # targets = ['PRD', 'RAB', 'SAS']
-    item = messages_inview[-1].text.split('\n')
-    message, target = item[:-1], item[-1]
-    targets = target.split(' ')
 
-    timestamp = messages_datetime_inview[-1].text
-    timestamp = datetime.strptime(timestamp, "%I:%M %p").time()
+    item = []
+    message, targets = [], []
+    # If there are no messages initially in Slave group, return None, None
+    timestamp = -1
+    if len(messages_inview) > 0:
+        item = messages_inview[-1].text.split('\n')
+        message, target = item[:-1], item[-1]
+        targets = target.split(' ')
+        timestamp = messages_datetime_inview[-1].text
+        timestamp = datetime.strptime(timestamp, "%I:%M %p").time()
+    else:
+        return None, None
 
     # Make a Content object for the above message so that it
     # can be sent to targets using send()
@@ -262,54 +236,156 @@ def logout(driver):
         driver.close()
 
 
+# Helper function for get_all_users
+# returns the list of names present in the search query result
+# of get_all_users
+def get_names(driver):
+    names = []
+    try:
+        names_elem = WebDriverWait(driver, 20).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, "//div[@class='chat-title']")))
+        names = [i.text for i in names_elem]
+    except TimeoutException as e:
+        print("Nothing found")
+    return set(names)
+
+
+# returns all the unique user names from the contact search feature
+def get_all_users(driver):
+
+    # New chat allows to get all the user names and avoids group names
+    # This finds and clicks New chat button
+    try:
+        new_chat = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//button[@title='New chat']")))
+        new_chat.click()
+        time.sleep(1)
+    except TimeoutException as e:
+        raise(Exception('New chat button not found'))
+
+    # After New chat clicked, this searches user names by typing all 2
+    # combinations of alphabets and get_names gives all the names which are
+    # visible on the screen
+    # A better approach would be to scroll through the contact names but i
+    # don't know how to do that :(
+    try:
+        all_usernames = set()
+        for i in range(26):
+            contact_search = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, "//input[@title='Search contacts']")))
+            contact_search.click()
+            time.sleep(1)
+            for j in range(26):
+                user_name = chr(ord('a') + i) + chr(ord('a') + j)
+                contact_search.send_keys(user_name)
+                time.sleep(1)
+                all_usernames |= get_usernames(driver)
+                try:
+                    close_search = WebDriverWait(driver, 20).until(
+                        EC.presence_of_element_located(
+                            (By.XPATH, "//button[@class='icon-close-search']")))
+                    close_search.click()
+                    time.sleep(1)
+                except TimeoutException as e:
+                    pass
+    except Exception as e:
+        print(e)
+
+    return all_usernames
+
+
+# Create a simplified ID - NAME dictionary, where ID is about
+# 3-5 characters and where first 3 characters are chosen from the NAME
+# and last 1-2 characters are numbers if NAMES collide
+def make_cheatsheet(driver):
+    usernames = get_all_users(driver)
+
+    targets = {}
+    for name in usernames:
+        first, second, *rest = name.split(' ')
+        key = first.upper()[:2]
+        if second:
+            key += second.upper()[0]
+        else:
+            key = first[2].upper()
+        count = 0
+        while key in targets:
+            if count:
+                key = key[:-1] + str(count)
+            else:
+                key += '1'
+            count += 1
+        targets[key] = name
+
+    # This is for sending to Master group for reference
+    item = Content('text')
+    item.message = []
+    for key, value in sorted(targets.items()):
+        msg = "{:<15} {:<15}".format(key, value)
+        item.message.append(msg)
+    item.timestamp = datetime.ctime(datetime.now())
+    return targets, item
+
+
 # Driver program which will run this whole system
 def main():
     # If metadata exists then get data from that that else
     # create data with initial values
-    if os.path.isfile('whatsapp.pkl'):
-        with open('whatsapp.pkl') as meta:
+    meta_file = 'whatsapp.pkl'
+    if os.path.isfile(meta_file):
+        with open(meta_file, 'rb') as meta:
             data = pickle.load(meta)
     else:
         data = {
+            "targets": [],
             "allSentMessages": [],
             "lastSentMessageTime": -1
         }
 
     # Pause time between messages
-    PAUSE = 5 * 60
+    PAUSE = 15 * 60
+    REPEAT = 3
     try:
         driver = login()
         target = 'Master'
+        if len(data['targets']) == 0:
+            data['targets'], cheat_content = make_cheatsheet(driver)
+            try:
+                send(driver, 'Master', cheat_content)
+            except Exception as e:
+                raise(e)
         while True:
-            for i in range(5):
+            for i in range(REPEAT):
                 try:
                     site = random.choice(ALLSITES)
-                    if site == 'xkcd':
-                        item = xkcd.get_image()
-                        print("In xkcd")
-                    elif site == 'brainyquote':
-                        item = brainyquote.get_quote()
-                        print("In Brainyquote")
+                    item = site.get_content()
                     send(driver, target, item)
                 except Exception as e:
                     print(e)
                 time.sleep(PAUSE)
             item, ids = recieve(driver, 'Slave')
-            if data['lastSentMessageTime'] == -1 \
-                    or item.timestamp > data['lastSentMessageTime']:
-                if ids[0] == 'ALL':
-                    ids = TARGETS.keys()
-                for id_ in ids:
-                    try:
-                        send(driver, TARGETS[id_], item)
-                        data['allSentMessages'].append(item)
-                    except Exception as e:
-                        print(e)
-                data['lastSentMessageTime'] = item.timestamp
+            if item:
+                if data['lastSentMessageTime'] == -1 \
+                        or item.timestamp > data['lastSentMessageTime']:
+                    if ids[0] == 'ALL':
+                        ids = data['targets'].keys()
+                    elif ids[0] == 'QUIT':
+                        break
+                    for id_ in ids:
+                        try:
+                            send(driver, data['targets'][id_], item)
+                            data['allSentMessages'].append(item)
+                        except Exception as e:
+                            print(e)
+                    data['lastSentMessageTime'] = item.timestamp
     except KeyboardInterrupt:
-        with open('whatsapp.pkl', 'wb') as meta:
-            pickle.dump(data, meta)
+        print('Program Ended')
     finally:
+        with open(meta_file, 'wb') as meta:
+            pickle.dump(data, meta)
         logout(driver)
 
 
